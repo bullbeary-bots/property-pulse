16b2bba7f1478726ba6ae70c4931557a62cd70a6
bullbeary-bots <bullbeary.bots@outlook.com>
2025-05-12 21:05:05 +0200

    Added fetch properties

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,9 @@
 import Hero from "@/components/Hero";
 import HomeProperties from "@/components/HomeProperties";
 import InfoBoxes from "@/components/InfoBoxes";
-import Link from "next/link";
 
 const HomePage = () => {
+
   return (
     <>
     <Hero />
